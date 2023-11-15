63a9295e24c33e7fb85a03a8ddb662d8d0209873
Nandiniar <nandiniarora338@gmail.com>
2023-11-15 14:47:03 +0000

    Code Synced by DhiWise

---
+++ android/app/src/main/kotlin/com/nandinisapplication/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nandinisapplication.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/apiClient/api_client.dart
@@ -0,0 +1,3 @@
+import 'package:nandini_s_application1/core/app_export.dart';
+
+class ApiClient {}
